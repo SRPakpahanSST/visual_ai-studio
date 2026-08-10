5f7023cefc53f78b66c077db71d744b2bf52140a
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-08-10 10:07:44 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from .generator import generate_simple_image, generate_advanced_image
+from .image_generator import generate_simple_image, generate_advanced_image, get_pipeline
 from .inpainter import inpaint_engine, create_auto_mask
-from .outpainter import prepare_outpainting, generate_outpainting
-from .scheduler import load_scheduler
+from .outpainter import generate_outpainting
 from .batch import batch_inference
+from .scheduler import load_scheduler
 from .helpers import display_grid, save_image
