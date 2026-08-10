398a7ce814ebca6812784c848bf84db1cb0e9cd3
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-08-10 10:13:32 +0700

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -4,18 +4,22 @@ import os
 
 def display_grid(images, titles=None, rows=1, cols=4, figsize=(20, 5)):
     fig, axes = plt.subplots(rows, cols, figsize=figsize)
+    
     if rows == 1 and cols == 1:
         axes = [axes]
     else:
         axes = axes.flatten()
+    
     for i, img in enumerate(images):
         if i < len(axes):
             axes[i].imshow(img)
             axes[i].axis('off')
             if titles and i < len(titles):
                 axes[i].set_title(titles[i], fontsize=10)
+    
     for j in range(len(images), len(axes)):
         axes[j].axis('off')
+    
     plt.tight_layout()
     return fig
 
