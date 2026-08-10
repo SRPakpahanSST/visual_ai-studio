bf9859506c11eba63b22a82f82989d52b2dbc763
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-08-10 10:12:57 +0700

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,15 +1,27 @@
-from diffusers import EulerAncestralDiscreteScheduler, DPMSolverMultistepScheduler, DDIMScheduler
+from diffusers import (
+    EulerAncestralDiscreteScheduler,
+    DPMSolverMultistepScheduler,
+    DDIMScheduler
+)
 
 def load_scheduler(pipe, scheduler_name: str):
     scheduler_name = scheduler_name.lower()
     config = pipe.scheduler.config
+    
     if scheduler_name == "euler_a":
-        new_scheduler = EulerAncestralDiscreteScheduler.from_config(config, use_karras_sigmas=True)
+        new_scheduler = EulerAncestralDiscreteScheduler.from_config(
+            config, use_karras_sigmas=True
+        )
     elif scheduler_name == "dpm++":
-        new_scheduler = DPMSolverMultistepScheduler.from_config(config, use_karras_sigmas=True)
+        new_scheduler = DPMSolverMultistepScheduler.from_config(
+            config, use_karras_sigmas=True,
+            algorithm_type="dpmsolver++", solver_type="midpoint",
+            final_sigmas_type="zero"
+        )
     elif scheduler_name == "ddim":
         new_scheduler = DDIMScheduler.from_config(config)
     else:
         raise ValueError(f"Scheduler '{scheduler_name}' tidak dikenal. Gunakan: euler_a, dpm++, ddim")
+    
     pipe.scheduler = new_scheduler
     return pipe
